50feab5f86135d81cfde5fd8c935dd209a14bc9e
Falya Aqiela Sekardina <falyasekardina@gmail.com>
2020-09-27 22:05:46 +0700

    add  unit test

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/myapp/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/myapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
+    private var count : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,11 +14,25 @@ class MainActivity : AppCompatActivity() {
         textView.text = count.toString() + " Clicks!"
 
         increment.setOnClickListener{
-            textView.text = (++count).toString() + " Clicks!"
+            changeNumber(count, "plus")
+            textView.text = count.toString() + " Clicks!"
         }
 
         decrement.setOnClickListener{
-            textView.text = (--count).toString() + " Clicks!"
+            changeNumber(count, "minus")
+            textView.text = count.toString() + " Clicks!"
         }
     }
+
+    fun changeNumber(num : Int, type : String): NumResult {
+        if (type == "plus") {
+            count = num + 1
+        }
+        else {
+            count = num - 1
+        }
+        return NumResult(count)
+    }
+
+    data class NumResult(val num : Int)
 }
